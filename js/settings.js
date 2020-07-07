39446d4bb1a07764a3df9dff4c85999ff69cbfe0
Dizzienoo <32038070+Dizzienoo@users.noreply.github.com>
2020-07-07 18:20:47 +0100

    set to 15

--- js/settings.js
+++ js/settings.js
@@ -102,7 +102,7 @@ export const globalState = new function () {
 	},
 	// This variable is used to set the position of the rocks and the size of the power beam
 	// 4 would mean the power size is a quarter of the screen
-	this.powerSize = 12
+	this.powerSize = 15
 	// Time in ms before trial is moved along
 	this.trialTime = 5000;
 	// How long left before the trial auto completes (in seconds)
